4aea0c80be1d259523e88f6af14ab9c6bcd0ba12
Jake Gage <jake.gage@gmail.com>
2015-05-14 18:54:31 -0500

    adds sorting for package service

--- src/main/groovy/com/github/dispader/gwite/controller/PackageService.groovy
+++ src/main/groovy/com/github/dispader/gwite/controller/PackageService.groovy
@@ -8,7 +8,7 @@ class PackageService {
     @Produces(['application/json']) @GET def get() {
         def runtimePackages = ClassLoader.getClassLoader(this.class).getPackages().collect() { it.name }
         def parentPackages = ClassLoader.getClassLoader(this.class).parent.getPackages().collect() { it.name }
-        runtimePackages - parentPackages
+        (runtimePackages - parentPackages).sort()
     }
     
 }
